ad3118ea20ae9b3f880fd7ce5ac2a1f41dc4b5d9
fobossalmeron <fobos.salmeron@gmail.com>
2019-07-31 00:50:40 +0200

    Updates

---
+++ precache-manifest.17a620f5b9a98dd77ae89fa133662905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806cb21d0867e82a09f0fd7c05ae299f",
+    "url": "/acueducto_tests/index.html"
+  },
+  {
+    "revision": "82e88e7ba014ee849e26",
+    "url": "/acueducto_tests/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9feed1c948e49c891671",
+    "url": "/acueducto_tests/static/js/2.2314a3f3.chunk.js"
+  },
+  {
+    "revision": "82e88e7ba014ee849e26",
+    "url": "/acueducto_tests/static/js/main.168817cb.chunk.js"
+  },
+  {
+    "revision": "9f9bb41fb14649dbac9e",
+    "url": "/acueducto_tests/static/js/runtime~main.8d93dd2f.js"
+  },
+  {
+    "revision": "595973c56de544654ea11bdbed96c0ec",
+    "url": "/acueducto_tests/static/media/logo.595973c5.svg"
+  }
+]);
